6ef660d667bced5175278daaf65ed25be53c5dad
Vladislav Maksimovic <daksrapsody@gmail.com>
2025-02-08 09:21:54 +0100

    feature: initial setup enchacements

--- dist/transactions/dto/transaction.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class TransactionDto {
-    transactionHash: string;
-    blockNumber: number;
-    timestamp: Date;
-    transactionIndex: number;
-    sender: string;
-    receiver: string;
-    amount: number;
-}
--- dist/transactions/entities/transactions.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Transaction {
-    id: number;
-    transactionHash: string;
-    timestamp: Date;
-    blockNumber: number;
-    sender: string;
-    receiver: string;
-    amount: number;
-}
